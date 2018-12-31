e835450bef30c1af85489363b2e2e690e92d5f71
Andrew Peacock <ajpeacock0@gmail.com>
2018-12-30 19:42:59 -0800

    Fixing refactor mistakes

--- install_tools.sh
+++ install_tools.sh
@@ -7,6 +7,3 @@ git clone --depth 1 http://github.com/junegunn/fzf.git ~/.fzf
 # Install the RipGrep tool
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
 sudo dpkg -i ripgrep_0.10.0_amd64.deb
-
-# Install the gnome Vim version which has clipboard support
-sudo apt-get install vim-gnome
--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install the gnome Vim version which has clipboard support
+sudo apt-get install vim-gnome
