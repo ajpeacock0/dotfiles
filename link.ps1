0dfe12c8108298a2684ce9e4346f32a924dd0578
Andrew Peacock <ajpeacock0@gmail.com>
2019-01-12 13:27:16 -0800

    Fixing Powershell linking scripts to include the `ln` alias

--- link.ps1
+++ link.ps1
@@ -1,3 +1,7 @@
+# Include Powershell functions, needed for the `ln` alias
+# You must first run the link.ps1 script for powershell
+. ~\.powershell\functions.ps1
+
 # Link the .gitconfig to home. TODO: Change assumed "git_repos\dotfiles" path to use scriptDirectory
 ln "${Env:UserProfile}\git_repos\dotfiles\.gitconfig" "${Env:UserProfile}\.gitconfig"
 
--- powershell/link.ps1
+++ powershell/link.ps1
@@ -1,3 +1,6 @@
+# Include Powershell functions, needed for the `ln` alias
+. .\.powershell\functions.ps1
+
 # Link the .powershell config files to home. TODO: Change assumed "git_repos\dotfiles" path to use scriptDirectory
 ln "${Env:UserProfile}\git_repos\dotfiles\powershell\.powershell" "${Env:UserProfile}\.powershell"
 
