59b30ebda8a9343ea408c2b66b451dfb57bedf37
Andrew Peacock <ajpeacock0@gmail.com>
2019-02-09 17:48:32 -0800

    Adding the fd tool to install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -11,6 +11,10 @@ git clone https://github.com/rupa/z.git ~/.z
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
 sudo dpkg -i ripgrep_0.10.0_amd64.deb
 
+# Install the fd tool
+curl -LO https://github.com/sharkdp/fd/releases/download/v7.2.0/fd_7.2.0_amd64.deb
+sudo dpkg -i fd_7.2.0_amd64.deb
+
 # Install `thefuck` tool
 sudo apt install python3-dev python3-pip python3-setuptools
 sudo pip3 install thefuck
