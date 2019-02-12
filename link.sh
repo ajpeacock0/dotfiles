e826fd3bbf724012ebc0231704ea8093d4ea0211
Andrew Peacock <ajpeacock0@gmail.com>
2019-02-12 14:19:32 -0800

    Adding gpakosz's tmux conf installation

--- link.sh
+++ link.sh
@@ -11,4 +11,11 @@ ln -f -s $SCRIPTPATH/vim/.vimrc ~/.vimrc
 ln -f -s $SCRIPTPATH/zsh/.zshrc ~/.zshrc
 ln -f -s $SCRIPTPATH/.gitconfig ~/.gitconfig
 ln -f -s $SCRIPTPATH/.inputrc ~/.inputrc
+
+# Download a shared tmux config
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+# Link the included .tmux.conf
+ln -f -s ~/.tmux.conf ~/.tmux.conf
+# Link my local tmux.conf overrides and additions
 ln -f -s $SCRIPTPATH/.tmux.conf.local ~/.tmux.conf.local
+
