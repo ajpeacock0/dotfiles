b20c33074bd4d271ece0fca90c8773af6a20f77d
Andrew Peacock <ajpeacock0@gmail.com>
2020-05-16 13:21:53 -0700

    Removing dependence on gpakosz tmux config

--- link.sh
+++ link.sh
@@ -12,11 +12,5 @@ ln -f -s $SCRIPTPATH/.zshrc ~/.zshrc
 ln -f -s $SCRIPTPATH/.p10k.zsh ~/.p10k.zsh
 ln -f -s $SCRIPTPATH/.gitconfig ~/.gitconfig
 ln -f -s $SCRIPTPATH/.inputrc ~/.inputrc
-
-# Download a shared tmux config
-git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-# Link the included .tmux.conf
-ln -f -s ~/.tmux/.tmux.conf ~/.tmux.conf
-# Link my local tmux.conf overrides and additions
-ln -f -s $SCRIPTPATH/.tmux.conf.local ~/.tmux.conf.local
-ln -f -s $SCRIPTPATH/tmuxline_lightline ~/.tmuxline_lightline
+ln -f -s $SCRIPTPATH/.tmux.conf ~/.tmux.conf
+ln -f -s $SCRIPTPATH/.tmuxline_lightline ~/.tmuxline_lightline
