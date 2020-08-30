b1e6bdd0a5ff670111b5b8a296034009b6c77d93
Andrew Peacock <ajpeacock0@gmail.com>
2020-08-30 09:50:00 -0700

    Download vim-plug for NeoVim

--- install_tools.ps1
+++ install_tools.ps1
@@ -4,19 +4,33 @@
 scoop install vim neovim fd fzf ripgrep git sudo
 
 # Install FiraCode from NerdFont
-scoop bucket add nerd-font
+scoop bucket add nerd-fonts
 sudo scoop install FiraCode-NF
 
 # Install XLaunch from extras vcxsrv
 scoop bucket add extras
 scoop install vcxsrv extras/vcredist2015
 
-# Install Vim-Plug
+# Install Vim-Plug for Vim
 md ~\vimfiles\autoload
 $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile(
   $uri,
   $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
     "~\vimfiles\autoload\plug.vim"
+  )
+)
+
+# Install Vim-Plug for NeoVim
+md ~\AppData\Local\nvim
+touch ~\AppData\Local\nvim\init.vim
+
+# Install plugin-manager vim-plug
+$ md ~\AppData\Local\nvim\autoload
+$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+(New-Object Net.WebClient).DownloadFile(
+  $uri,
+  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+    "~\AppData\Local\nvim\autoload\plug.vim"
   )
 )
