73a7800f92387f174d776932a3400c6d402c358f
Andrew Peacock <ajpeacock0@gmail.com>
2020-10-09 19:44:04 -0700

    Adding a Join-Files function

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -30,6 +30,8 @@ Import-Module ZLocation
 Set-Alias ll Get-ChildItemColor -Option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 
+function Join-Files { Get-ChildItem -File | % { echo $_.Name; cat $_.Name > joined_files } }
+
 # Set '~' location to the User Profile directory
 function cusorprofile { Set-Location ~ }
 Set-Alias ~ cusorprofile -Option AllScope
