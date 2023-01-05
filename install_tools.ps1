ac266b5abf24d9ac2d98d1a5ba3a65e578650d17
Andrew Peacock <ajpeacock0@gmail.com>
2023-01-05 15:25:18 -0800

    Updating Install Tools script to use functions

--- install_tools.ps1
+++ install_tools.ps1
@@ -1,55 +1,118 @@
-# Install the latest vim from scoop
-# Along with the tools fd and fzf for
-# file searching
-scoop install vim neovim fd fzf ripgrep git sudo python bat zoxide jq
+[CmdletBinding(DefaultParameterSetName = 'None', SupportsShouldProcess)]
+param(
+    [Parameter(Mandatory = $true, ParameterSetName = "Scoop")]
+    [switch] $Scoop = $false,
 
-# pynvim is needed for Vim to use Python
-python3 -m pip install --user --upgrade pynvim
+    [Parameter(Mandatory = $true, ParameterSetName = "Vim")]
+    [switch] $Vim = $false,
 
-# Install FiraCode from NerdFont
-scoop bucket add nerd-fonts
-sudo scoop install FiraCode-NF
-
-# Install XLaunch from extras vcxsrv
-scoop bucket add extras
-scoop install vcxsrv extras/vcredist2015
-
-# Include Powershell functions, needed for the `touch` alias
-. powershell\.powershell\functions.ps1
-
-# Create config file for NeoVim if it doesn't already exist
-$Script:NeoVimHome = "~\AppData\Local\nvim"
-if (-not (Test-Path $Script:NeoVimHome)) {
-    md $Script:NeoVimHome
-}
-touch "${Script:NeoVimHome}\init.vim"
-
-# Install Vim-Plug for NeoVim
-if (-not (Test-Path "${Script:NeoVimHome}\autoload")) {
-    md "${Script:NeoVimHome}\autoload"
-}
-$Script:uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-(New-Object Net.WebClient).DownloadFile(
-  $Script:uri,
-  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
-    "${Script:NeoVimHome}\autoload\plug.vim"
-  )
+    [Parameter(Mandatory = $true, ParameterSetName = "Defender")]
+    [switch] $Defender = $false
 )
 
-# Install Vim-Plug for Vim
-$Script:VimHome = "~\vimfiles"
-if (-not (Test-Path "${Script:VimHome}\autoload")) {
-    md "${Script:VimHome}\autoload"
+
+function Install-BasicTools {
+    [CmdletBinding(SupportsShouldProcess)]
+    param ()
+
+    # Install the latest vim from scoop
+    # Along with the tools fd and fzf for
+    # file searching
+    scoop install vim neovim fd fzf ripgrep git sudo python bat zoxide jq
+
+    # Install FiraCode from NerdFont
+    scoop bucket add nerd-fonts
+    scoop install FiraCode-NF
+
+    # Install XLaunch from extras vcxsrv
+    scoop bucket add extras
+    scoop install vcxsrv extras/vcredist2015
+
+    # Install On-My-Posh (previously a Powershell Module)
+    scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
 }
-(New-Object Net.WebClient).DownloadFile(
-  $Script:uri,
-  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
-    "${Script:VimHome}\autoload\plug.vim"
-  )
-)
 
-# Install On-My-Posh (previously a Powershell Module)
-scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+function Include-PowershellFunctions {
+    [CmdletBinding(SupportsShouldProcess)]
+    param ()
 
-# Install the prefered Font
-scoop install FiraCode-NF
+    # Include Powershell functions
+    . powershell\.powershell\functions.ps1
+}
+
+function Setup-Vim {
+    [CmdletBinding(SupportsShouldProcess)]
+    param ()
+
+    # Needed for the `touch` alias
+    Include-PowershellFunctions
+
+    # pynvim is needed for Vim to use Python
+    python3 -m pip install --user --upgrade pynvim
+
+    # Create config file for NeoVim if it doesn't already exist
+    $Script:NeoVimHome = "~\AppData\Local\nvim"
+    if (-not (Test-Path $Script:NeoVimHome)) {
+        md $Script:NeoVimHome
+    }
+    touch "${Script:NeoVimHome}\init.vim"
+
+    # Install Vim-Plug for NeoVim
+    if (-not (Test-Path "${Script:NeoVimHome}\autoload")) {
+        md "${Script:NeoVimHome}\autoload"
+    }
+    $Script:uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    (New-Object Net.WebClient).DownloadFile(
+      $Script:uri,
+      $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+        "${Script:NeoVimHome}\autoload\plug.vim"
+      )
+    )
+
+    # Install Vim-Plug for Vim
+    $Script:VimHome = "~\vimfiles"
+    if (-not (Test-Path "${Script:VimHome}\autoload")) {
+        md "${Script:VimHome}\autoload"
+    }
+    (New-Object Net.WebClient).DownloadFile(
+      $Script:uri,
+      $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+        "${Script:VimHome}\autoload\plug.vim"
+      )
+    )
+}
+
+function Add-DefenderExclusions {
+    [CmdletBinding(SupportsShouldProcess)]
+    param ()
+
+    # Stop windows Defener from Scanning Visual Studio
+    $VsWhereCommand = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    $VsInstances = & $VsWhereCommand -prerelease -all -format json | ConvertFrom-Json
+    foreach ($VsInstance in $VsInstances) {
+        sudo Add-MpPreference -ExclusionPath $VsInstance.installationPath
+    }
+
+    $Script:GitReposPath = "~\git_repos"
+    sudo Add-MpPreference -ExclusionPath $GitReposPath
+
+    $Script:OneDrivePath = "~\OneDrive"
+    sudo Add-MpPreference -ExclusionPath $OneDrivePath
+}
+
+$All = $PSCmdlet.ParameterSetName -eq 'None'
+
+if ($BasicTools -or $All) {
+    Write-Output "Installing basic scoop packages..."
+    Install-BasicTools
+}
+
+if ($Vim -or $All) {
+    Write-Output "Setting up Vim..."
+    Setup-Vim
+}
+
+if ($Defender -or $All) {
+    Write-Output "Adding Windows Defender Exclusions..."
+    Add-DefenderExclusions
+}
