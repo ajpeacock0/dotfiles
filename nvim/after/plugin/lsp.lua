49538a1101fbaba002331fed484fd23e902cd05e
Andrew Peacock <ajpeacock0@gmail.com>
2023-06-10 12:51:12 -0700

    Adding mason LSP config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,3 +1,18 @@
+require('mason').setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+require('mason-lspconfig').setup({
+    -- A list of servers to automatically install if they're not already installed
+    ensure_installed = { 'lua_ls', 'csharp_ls', 'marksman' },
+})
+
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
