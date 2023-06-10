129e3db2dbe49fd8118ba87a3bae3c36af9c6e7b
Andrew Peacock <ajpeacock0@gmail.com>
2023-06-10 13:15:20 -0700

    Removing marksman from mason

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({
 
 require('mason-lspconfig').setup({
     -- A list of servers to automatically install if they're not already installed
-    ensure_installed = { 'lua_ls', 'csharp_ls', 'marksman' },
+    ensure_installed = { 'lua_ls', 'csharp_ls' },
 })
 
 local lsp = require("lsp-zero")
