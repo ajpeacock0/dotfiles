2f6cc5facc5aaaddd651085239cbaef92a40e5e7
Andrew Peacock <ajpeacock0@gmail.com>
2023-06-10 13:24:56 -0700

    Removing csharp from mason

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({
 
 require('mason-lspconfig').setup({
     -- A list of servers to automatically install if they're not already installed
-    ensure_installed = { 'lua_ls', 'csharp_ls' },
+    ensure_installed = { 'lua_ls' },
 })
 
 local lsp = require("lsp-zero")
