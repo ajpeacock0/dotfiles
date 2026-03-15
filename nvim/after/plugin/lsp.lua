60f20adb1718fcb4b66b3bef33179952b20bbc1a
Andrew Peacock <ajpeacock0@gmail.com>
2026-03-15 14:21:18 -0700

    Fix lsp for nvim 0.11

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -13,26 +13,53 @@ require('mason-lspconfig').setup({
     ensure_installed = { 'lua_ls' },
 })
 
-local lsp = require("lsp-zero")
+-- Set up LSP keymaps when a language server attaches
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local opts = { buffer = args.buf }
+        vim.keymap.set('n', 'K',  vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+        vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
+        vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+    end,
+})
 
-lsp.preset("recommended")
+-- Auto-configure and enable all mason-installed LSP servers
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+for _, server_name in ipairs(require('mason-lspconfig').get_installed_servers()) do
+    vim.lsp.config(server_name, { capabilities = capabilities })
+    vim.lsp.enable(server_name)
+end
 
+-- nvim-cmp setup
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-  ["<C-Space>"] = cmp.mapping.complete(),
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+    }),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-k>']     = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-j>']     = cmp.mapping.select_next_item(cmp_select),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
 })
-
--- disable Enter to confirm
-cmp_mappings['<CR>'] = nil
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
-lsp.setup()
--- nvim/lua/ajpeacock0/packer.lua
+++ nvim/lua/ajpeacock0/packer.lua
@@ -19,22 +19,19 @@ return require('packer').startup(function(use)
 
     -- LSP
     use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+        -- LSP Support
+        'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-        }
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
+        'L3MON4D3/LuaSnip',
     }
 
     -- Commenting
